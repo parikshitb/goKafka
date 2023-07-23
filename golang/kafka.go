0eef4c03dce6f1d3f377774503fe49de68752c2f
parikshit <parixit23@gmail.com>
2023-07-23 12:25:59 +0900

    go module is added to consume kafka message. go module is able to connect to kafka broker but ReadMessage doesnt read actual values. Insted some index like 0, 1, 2 is read

---
+++ golang/kafka.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"time"
+	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
+)
+
+func main() {
+
+	con, err := kafka.NewConsumer(&kafka.ConfigMap{
+		"bootstrap.servers": "broker_one:9092",
+		"group.id": "gokafka",
+		"auto.offset.reset": "earliest",
+	})
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	
+	con.SubscribeTopics([]string{"http_logs"}, nil)
+
+	run := true
+	for run {
+		 msg, err := con.ReadMessage(time.Second)
+		 if err == nil {
+			fmt.Println(msg)
+		 } else if !err.(kafka.Error).IsTimeout() {
+			fmt.Printf("Consumer error: %v (%v)\n", err, msg)
+		 }
+	}
+	con.Close()
+}
